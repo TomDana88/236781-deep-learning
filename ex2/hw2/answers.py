d4a3c0fa23fcb68b875d2b74a0f6a39b07e101d1
ehud <gordonehud@gmail.com>
2024-02-24 22:01:33 +0200

    finished part1

--- ex2/hw2/answers.py
+++ ex2/hw2/answers.py
@@ -11,25 +11,23 @@ math (delimited with $$).
 part1_q1 = r"""
 **Your answer:**
 ## 1
-* A. The Jacobian for a single sample would have the shape $ (512,1024) $. Since we have $N=64$ samples, the Jacobian would have the shape $(64,512,1024)$.
-* B.  No, the above Jacobian will not be sparse. This is because: consider a single sample  $x$  and a single sample output $y$. Then $\frac{\partial y_i}{\partial x_j} = W_{ij}$, so the Jacobian is the weight matrix $W$. Since the weight matrix is not sparse, the Jacobian is not sparse as well.
-* C. 
-
+* A. When $\boldsymbol{Y}$ is considers a function of $\boldsymbol{X}$, then for a single sample, $\pderiv{\mat{Y}}{\mat{X}}$  would have the shape $ (512,1024) $. Since we have $N=64$ samples, then $\pderiv{\mat{Y}}{\mat{X}}$  would have the shape $(64,512,1024)$.
+* B.  No, the above Jacobian is not (generally) sparse. This is because: consider a single sample  $x$  and a single sample output $y$. Then $\frac{\partial y_i}{\partial x_j} = W_{ij}$, so the Jacobian is the weight matrix $W$. Since the weight matrix is not sparse, the Jacobian is not sparse as well.
+* C. No, Using the chain rule, we get:
+$$\delta \boldsymbol{X} = \delta \boldsymbol{Y} \frac{\partial \boldsymbol{Y}}{\partial \boldsymbol{X}} $$
+Notice we don't even have to store the entire Jacobian in memory, but rather, we can compute each column of $\frac{\partial \boldsymbol{Y}}{\partial \boldsymbol{X}} $, and evalute the inner product of that column with $\delta \boldsymbol{Y}$, and thus compute $\delta \boldsymbol{X}$ increcementally, without storing the entire Jacobian matrix.
 ## 2
+* A. Let's start with a single sample. When $\boldsymbol{Y}$ is considered a function of $\boldsymbol{W}$, then $\pderiv{\mat{Y}}{\mat{W}}$ has the shape $(512,1024, 512)$ . This is because for each component $y_i$ is a scalar-valued function, that gets an input $\boldsymbol{W}$ of dimensions $(512,1024)$, and so $\pderiv{y_i}{\mat{W}}$  is of shape $(512,1024)$, and since $\vec{Y}$ is of dim $512$, we have that $\frac{\partial \boldsymbol{Y}}{\partial \boldsymbol{W}}$ is a  3-tensor of dimensions $(512,1024,512)$. For $N=64$ samples, we have  that the shape of $\frac{\partial \boldsymbol{Y}}{\partial \boldsymbol{W}}$  is $(64,512,1024,512)$.
 
+* B. The above Jacobian we've described would be mostly sparse. Since $\vec{Y}=\vec{X}\mat{W}^T$,  $y_i$ only depends on the $i$-th row of $\mat{W}$, and zeroes for all other rows.
+* C. No, Using the chain rule, we get:
+$$\delta \boldsymbol{W} = \delta \boldsymbol{Y} \frac{\partial \boldsymbol{Y}}{\partial \boldsymbol{W}} $$
+Notice we don't even have to store the entire Jacobian in memory, but rather, we can compute $\frac{\partial \boldsymbol{Y}}{\partial \boldsymbol{W}} $, and evalute the inner product of that column with $\delta \boldsymbol{Y}$, and thus compute $\delta \boldsymbol{W}$ increcementally, without storing the entire Jacobian matrix.
 """
 
 part1_q2 = r"""
 **Your answer:**
-The above Jacobian we've described would be mostly sparse, 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+**NO**, back-propagation is not necessary for descent-based optimization, it is just an efficient way to compute the gradients of the loss function with respect to the weights, using the chain rule. There are other ways, like evolutionary algorithms, that optimize the weights using feedback. There's also numerical differentiation, which approxiamtes the gradient by averaging the loss over multiple points.
 """
 
 
